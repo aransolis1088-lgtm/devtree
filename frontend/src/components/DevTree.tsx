94bbe7a4081b38f77ef7252e7de19aadca4e0df2
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-03-09 22:11:45 -0600

    Implementación de Drag N Drop de redes sociales

--- frontend/src/components/DevTree.tsx
+++ frontend/src/components/DevTree.tsx
@@ -1,9 +1,14 @@
 import { Link, Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
+import { DndContext, closestCenter } from '@dnd-kit/core'
+import type {DragEndEvent} from '@dnd-kit/core'
+import { SortableContext, verticalListSortingStrategy, arrayMove } from '@dnd-kit/sortable'
+
 import NavigationTabs from "../components/NavigationTabs";
 import type { SocialNetwork, User } from "../types";
 import { useEffect, useState } from "react";
 import DevTreeLink from "./DevTreeLink";
+import { useQueryClient } from "@tanstack/react-query";
 
 type DevTreeProps = {
     data: User
@@ -15,6 +20,30 @@ export default function DevTree({ data }: DevTreeProps) {
     useEffect(() => {
         setEnabledLinks(JSON.parse(data.links).filter((item: SocialNetwork) => item.enabled))
     }, [data])
+
+    const queryClient = useQueryClient()
+
+    const handleDragEnd = (e: DragEndEvent) => {
+        const { active, over } = e
+
+        if (over && over.id) {
+            const prevIndex = enabledLinks.findIndex(link => link.id === active.id)
+            const newIndex = enabledLinks.findIndex(link => link.id === over.id)
+            const order = arrayMove(enabledLinks, prevIndex, newIndex)
+            setEnabledLinks(order)
+
+            const disabledLinks : SocialNetwork[] = JSON.parse(data.links).filter((item: SocialNetwork) => !item.enabled)
+
+            const links = [...order, ...disabledLinks]
+
+            queryClient.setQueryData(['user'], (prevData: User) => {
+                return {
+                    ...prevData,
+                    links: JSON.stringify(links)
+                }
+            })
+        }
+    }
 
     return (
         <>
@@ -55,11 +84,24 @@ export default function DevTree({ data }: DevTreeProps) {
                             {data.image && <img src={data.image} alt="Imagen Perfil" className="mx-auto max-w-[250px]" />}
                             <p className="text-center text-lg font-black text-white">{data.description}</p>
 
-                            <div className="mt-20 flex flex-col gap-5">
-                                {enabledLinks.map(link => (
-                                    <DevTreeLink key={link.name} link={link} />
-                                ))}
-                            </div>
+                            <DndContext
+                                collisionDetection={closestCenter}
+                                onDragEnd={handleDragEnd}
+                            >
+                                <div className="mt-20 flex flex-col gap-5">
+                                    <SortableContext
+                                        items={enabledLinks}
+                                        strategy={verticalListSortingStrategy}
+                                    >
+                                        {enabledLinks.map(link => (
+                                            <DevTreeLink key={link.name} link={link} />
+                                        ))}
+
+                                    </SortableContext>
+                                </div>
+                            </DndContext>
+
+
                         </div>
                     </div>
                 </main>
--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -24,7 +24,7 @@ export default function LinkTreeView() {
 
   useEffect(() => {
     const updatedData = devTreeLinks.map(item => {
-      const userLink = JSON.parse(user.links).find((link : SocialNetwork) => link.name === item.name)
+      const userLink = JSON.parse(user.links).find((link: SocialNetwork) => link.name === item.name)
       if (userLink) {
         return { ...item, url: userLink.url, enabled: userLink.enabled }
       }
@@ -36,14 +36,9 @@ export default function LinkTreeView() {
   const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
     const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? { ...link, url: e.target.value } : link)
     setDevTreeLinks(updatedLinks)
-
-    queryClient.setQueryData(['user'], (prevData: User) => {
-      return {
-        ...prevData,
-        links: JSON.stringify(updatedLinks)
-      }
-    })
   }
+
+  const links: SocialNetwork[] = JSON.parse(user.links)
 
   const handleEnableLink = (socialNetwork: string) => {
     const updatedLinks = devTreeLinks.map(link => {
@@ -57,10 +52,57 @@ export default function LinkTreeView() {
       return link
     })
     setDevTreeLinks(updatedLinks)
+
+    let updatedItems: SocialNetwork[] = []
+
+    const selectedSocialNetwork = updatedLinks.find(link => link.name === socialNetwork)
+    if (selectedSocialNetwork?.enabled) {
+      const id = links.filter(link => link.id).length +1
+      if (links.some(link => link.name === socialNetwork)) {
+        updatedItems = links.map(link => {
+          if(link.name === socialNetwork){
+            return {
+              ...link,
+              enabled: true,
+              id
+            }
+          }else{
+            return link
+          }
+        })
+      } else {
+        const newItem = {
+          ...selectedSocialNetwork,
+          id
+        }
+        updatedItems = [...links, newItem]
+      }
+    } else {
+      const indexToUpdate = links.findIndex(link => link.name === socialNetwork)
+      updatedItems = links.map(link => {
+        if (link.name === socialNetwork) {
+          return {
+            ...link,
+            id: 0,
+            enabled: false
+          }
+        } else if (link.id > indexToUpdate && (indexToUpdate !==0 && link.id ===1)) {
+          return {
+            ...link,
+            id: link.id - 1
+          }
+        } else {
+          return link
+        }
+      })
+    }
+
+
+    //Almacenar en la base de datos
     queryClient.setQueryData(['user'], (prevData: User) => {
       return {
         ...prevData,
-        links: JSON.stringify(updatedLinks)
+        links: JSON.stringify(updatedItems)
       }
     })
   }
@@ -76,7 +118,7 @@ export default function LinkTreeView() {
             handleEnableLink={handleEnableLink}
           />
         ))}
-        <button className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded font-bold" onClick={() => mutate(user)}>Guardar Cambios</button>
+        <button className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded font-bold" onClick={() => mutate(queryClient.getQueryData(['user'])!)}>Guardar Cambios</button>
       </div>
     </>
   )
